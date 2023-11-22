14e12199867cabe18c98b4dc259d14dcbe6f4565
CristiGitHub <lefterioancristian@gmail.com>
2023-11-22 02:25:30 +0200

    MVP req

---
+++ projx-core/src/main/java/com/ProjX/projxcore/ProjxCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ProjX.projxcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjxCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjxCoreApplication.class, args);
+	}
+
+}
